48fd00c3110b4350e6cceb08443f7d66d8caddda
romankravchuk <kravchuk.roman02@gmail.com>
2023-02-03 23:29:50 +0800

    docs: added docs for product repository

--- internal/repository/product.repository.go
+++ internal/repository/product.repository.go
@@ -1,3 +1,4 @@
+// repository contains the implementation of a MongoDB repository for products
 package repository
 
 import (
@@ -10,11 +11,16 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
+// MongoProductRepository is a struct that implements product repository using MongoDB as storage.
 type MongoProductRepository struct {
-	db   *mongo.Database
+	// db is a reference to a MongoDB database.
+	db *mongo.Database
+
+	// coll is a string representing the name of the products collection in the database.
 	coll string
 }
 
+// NewProductRepository creates a new instance of MongoProductRepository and returns a pointer to it.
 func NewProductRepository(db *mongo.Database) *MongoProductRepository {
 	return &MongoProductRepository{
 		db:   db,
@@ -22,6 +28,12 @@ func NewProductRepository(db *mongo.Database) *MongoProductRepository {
 	}
 }
 
+// GetByID returns a product with a given ID.
+//
+// If a product with the given ID is not found, the error will be set to mongo.ErrNoDocuments.
+//
+// id is a string representation of the ObjectID of the product.
+// ctx is a context used to cancel the request.
 func (r *MongoProductRepository) GetByID(ctx context.Context, id string) (*models.Product, error) {
 	var (
 		product  = &models.Product{}
@@ -33,6 +45,13 @@ func (r *MongoProductRepository) GetByID(ctx context.Context, id string) (*model
 	return product, err
 }
 
+// GetAll returns a slice of all products that match the filter criteria, sorted according to the sort options, and paged.
+// If there are no products matching the filter criteria, an empty slice will be returned.
+//
+// filter is a pointer to a ProductFilter that specifies the filter criteria for the query.
+// sort is a pointer to a ProductSort that specifies the sort options for the query.
+// page is an integer representing the number of the page to return.
+// ctx is a context used to cancel the request.
 func (r *MongoProductRepository) GetAll(ctx context.Context, filter *filter.ProductFilter, sort *filter.ProductSort, page int) ([]*models.Product, error) {
 	aggregatePipline := r.buildProductPipline(filter, sort, page)
 	cursor, err := r.db.Collection(r.coll).Aggregate(ctx, aggregatePipline)
@@ -45,6 +64,10 @@ func (r *MongoProductRepository) GetAll(ctx context.Context, filter *filter.Prod
 	return products, err
 }
 
+// buildProductPipline creates a pipeline for aggregating products based on the filter and sort criteria, and the desired page number.
+// The pipeline consists of multiple stages, including filtering based on the provided criteria, sorting the results, skipping to the desired page, and limiting the results to 10 products.
+// The pipeline is represented as a slice of bson.M, which is a type used to represent a BSON document.
+// The function returns the created pipeline.
 func (r *MongoProductRepository) buildProductPipline(filter *filter.ProductFilter, sort *filter.ProductSort, page int) []bson.M {
 	aggregatePipline := make([]bson.M, 0)
 	if filter != nil {
@@ -64,6 +87,9 @@ func (r *MongoProductRepository) buildProductPipline(filter *filter.ProductFilte
 	return aggregatePipline
 }
 
+// buildProductFilter creates a filter document for aggregating products based on the provided filter criteria.
+// The filter document is represented as a bson.M, which is a type used to represent a BSON document.
+// The function returns the created filter document.
 func (r *MongoProductRepository) buildProductFilter(filter *filter.ProductFilter) bson.M {
 	filterDoc := bson.M{}
 	if filter.Category != "" {
@@ -85,6 +111,8 @@ func (r *MongoProductRepository) buildProductFilter(filter *filter.ProductFilter
 	return filterDoc
 }
 
+// buildProductSort creates a BSON document for sorting products in a mongodb collection.
+// It takes in a sort option (sort.ProductSort) and returns a BSON document (bson.M) to be used in the mongodb aggregation pipeline.
 func (r *MongoProductRepository) buildProductSort(sort *filter.ProductSort) bson.M {
 	sortDoc := bson.M{}
 	for _, opt := range sort.Options {
@@ -99,6 +127,9 @@ func (r *MongoProductRepository) buildProductSort(sort *filter.ProductSort) bson
 	return sortDoc
 }
 
+// Insert inserts a new product into the mongodb collection.
+// It takes in a context (context.Context) and a product instance (models.Product), inserts it into the collection,
+// and returns the inserted product instance with its ID set or an error.
 func (r *MongoProductRepository) Insert(ctx context.Context, product *models.Product) (*models.Product, error) {
 	res, err := r.db.Collection(r.coll).InsertOne(ctx, product)
 	if err != nil {
@@ -108,6 +139,9 @@ func (r *MongoProductRepository) Insert(ctx context.Context, product *models.Pro
 	return product, err
 }
 
+// Update updates a product in the mongodb collection.
+// It takes in a context (context.Context), the product ID (string), and a product instance (models.Product).
+// It updates the product in the collection and returns the updated product instance or an error.
 func (r *MongoProductRepository) Update(ctx context.Context, id string, p *models.Product) (*models.Product, error) {
 	var (
 		objID, _ = primitive.ObjectIDFromHex(id)
@@ -118,6 +152,8 @@ func (r *MongoProductRepository) Update(ctx context.Context, id string, p *model
 	return p, err
 }
 
+// Delete deletes a product from the mongodb collection.
+// It takes in a context (context.Context) and a product ID (string). It deletes the product from the collection and returns an error if there was an issue.
 func (r *MongoProductRepository) Delete(ctx context.Context, id string) error {
 	var (
 		objID, _ = primitive.ObjectIDFromHex(id)
